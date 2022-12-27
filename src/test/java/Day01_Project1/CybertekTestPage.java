d2f17234c6c905bd719915cfe568604e7c0cba84
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-28 01:20:57 +0300

    Created a Selenium Project, added dependencies into pom.xml. added template.txt file

---
+++ src/test/java/Day01_Project1/CybertekTestPage.java
@@ -0,0 +1,33 @@
+package Day01_Project1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CybertekTestPage {
+
+    public static void main(String[] args) {
+
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
