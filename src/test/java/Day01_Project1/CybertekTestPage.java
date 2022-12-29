1496f61da9514a25f202b60a508570540307d722
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-29 19:46:38 +0300

    driver.get done

--- src/test/java/Day01_Project1/CybertekTestPage.java
+++ src/test/java/Day01_Project1/CybertekTestPage.java
@@ -14,6 +14,24 @@ public class CybertekTestPage {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        //5-	Open webpage www.turkeyoutdoor.org
+        driver.get("https://www.turkeyoutdoor.org");
+
+        driver.close();
+
+
+        //	Commit and Push
+        //6-	Click on “Forum” link
+        //7-	Click “İsim” input box and write a name
+        //	Commit an push
+        //8-	Click “e-posta”  input box and write a email
+        //9-	Click “mesaj” input box and write a name
+        //	Commit and Push
+        //10-   Click the "Bize Yazın" button
+        //11-	 Verify the result box is contain "başarılı" Word
+        //	Commit and Push
+        //12- Take page the Previous Page
+        //13-Verify the Title is “Türkiye'nin Outdoor Sayfası”
 
 
 
